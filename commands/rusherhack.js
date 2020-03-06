dd4da589e01d7efbbbaf7e18b61810d4713d138e
Jamie <47989645+JamieUwU@users.noreply.github.com>
2020-03-05 17:18:24 -0800

    enabled rusherhack command but disabled the banning feature uwu

--- commands/rusherhack.js
+++ commands/rusherhack.js
@@ -5,10 +5,9 @@ module.exports = {
     nsfw: false,
 
     execute(bot, message, args) {
-        if (message.member.id !== "605843924657111082") return message.channel.send("Command is **disabled** currently rn!!");
         let bUser = message.author;
 
-        message.guild.member(bUser).ban("rekt rusherhack user!!!");
+        // message.guild.member(bUser).ban("rekt rusherhack user!!!");
         message.author.send("lol rekt but heres a code what you can use: beaner");
     }
 };
